7ee7ded15e3a07ac62282cef2db42ef5d566f0fe
Denis <Ori-wiki@gmail.com>
2023-07-16 03:36:28 +0300

    add validate

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { AuthPage } from './pages/auth-page';
 
 function App() {
   return (
     <div className='App'>
+      <AuthPage />
       <h1>Hello world</h1>
     </div>
   );
