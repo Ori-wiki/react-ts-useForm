3f930f9a6051b76f49f582f16f09fabb79700c7c
Denis <Ori-wiki@gmail.com>
2023-07-18 03:42:43 +0300

    add fireBase

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import '@fontsource/roboto/700.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import '../firebase';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
